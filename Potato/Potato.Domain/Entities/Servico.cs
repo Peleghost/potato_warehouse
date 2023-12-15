a08820f59190506f80d036a1ba2b6c5e0a299a83
MuriloSGomes <seno.murilo@hotmail.com>
2023-12-14 22:56:52 -0300

    Cria entidadeBase para herdar em todas as entidades Cria classe de servico Adiciona camada de validação

---
+++ Potato/Potato.Domain/Entities/Servico.cs
@@ -0,0 +1,46 @@
+﻿using Potato.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potato.Domain.Entities
+{
+    public class Servico : EntidadeBase
+    {
+        public Cliente Cliente { get; private set; }
+        public Peca Peca { get; private set; }
+        public string Veiculo { get; private set; }
+        public string Descricao { get; private set; }
+        public double Preco { get; private set; }
+
+        public Servico(Cliente cliente, Peca peca, string veiculo, string descricao, double preco)
+        {
+            this.Cliente = cliente;
+            this.Peca = peca;
+            this.Veiculo = veiculo;
+            this.Descricao = descricao;
+            this.Preco = preco;
+        }
+        public static Result<Servico> Criar(Cliente cliente, Peca peca, string veiculo, string descricao, double preco)
+        {
+            if (cliente == null)
+                return new Result<Servico>(isSuccess: false, null);
+
+            if (cliente == null)
+                return new Result<Servico>(isSuccess: false, null);
+
+            if (cliente == null)
+                return new Result<Servico>(isSuccess: false, null);
+
+            if (cliente == null)
+                return new Result<Servico>(isSuccess: false, null);
+
+            if (cliente == null)
+                return new Result<Servico>(isSuccess: false, null);
+
+            return new Result<Servico>(isSuccess: true, new Servico(cliente, peca, veiculo, descricao, preco));
+        }
+    }
+}
---
+++ Potato/Potato.Domain/Validation/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potato.Domain.Validation
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+
+        public Result(bool isSuccess, T value)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+        }
+    }
+}
